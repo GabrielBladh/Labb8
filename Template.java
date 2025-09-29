5ce1af1fdcd16e8e11581d5fbbce9e93fe534af2
GabrielBladh <gabrielbladh@hotmail.com>
2025-09-29 18:48:41 +0200

    template

---
+++ Template.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Template
+{
+    public static void meny(String[] args)
+    {
+        System.out.println("Select one:");
+        AddNumbers();
+    }
+    public static void AddNumbers()
+    {
+        System.out.println("1. Add numbers");
+    }
+    public static void Countdown()
+    {
+        System.out.println("Countdown");
+    }
+}
